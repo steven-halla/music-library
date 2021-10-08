a09646df1a4397b2c40fd50bb9ae075eb131e3e8
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-10-08 07:47:55 -0700

    "base project set up"

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { }
+  }
+  render() {
+  return(
+    <div>
+      <h1>Hello moto</h1>
+    </div>
+    );
+  }
+}
