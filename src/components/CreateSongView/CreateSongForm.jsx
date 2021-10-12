d972aa33ebeb1026e137946bf59fde05bae4266f
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-10-11 17:35:57 -0700

    "material ui added for buttons and input tags"

--- src/components/CreateSongView/CreateSongForm.jsx
+++ src/components/CreateSongView/CreateSongForm.jsx
@@ -1,20 +1,29 @@
 import React, {Component} from "react";
 import axios from "axios";
 import styled from "styled-components";
+import Button from '@mui/material/Button';
+import {Grid, TextField} from "@mui/material";
 
 
 const CreatesongFormDiv = styled.div`
-  background-color: aqua;
 
+    
+ Grid {
+   padding-left: 10px;
+ }
+  
+  
   label{
     display: table-cell;
-    text-align: right;
+    justify-content: flex-end;
   }
   input {
     display: table-cell;
+
   }
   div.row{
     display:table-row;
+
   }
   
 
@@ -61,21 +70,21 @@ export class CreateSongForm extends Component {
   render(){
     return(
       <CreatesongFormDiv>
-        <div>
+        <Grid container justifyContent="flex-end">
           <form action="" onSubmit={this.handleSubmit}>
-            <label htmlFor="">Artist</label>
-            <input name="artist" onChange={this.handleChange} type="text"/><br/>
-            <label htmlFor="">title</label>
-            <input name="title" onChange={this.handleChange} type="text"/><br/>
-            <label htmlFor="">album</label>
-            <input name="album" onChange={this.handleChange} type="text"/><br/>
-            <label htmlFor="">genre</label>
-            <input name="genre" onChange={this.handleChange} type="text"/><br/>
-            <label htmlFor="">release date</label>
-            <input name="release_date" onChange={this.handleChange} type="text"/><br/><br/>
-            <button type="submit">Create Song</button>
+            <label htmlFor=""> </label>
+            <TextField id="filled-basic" label="Artist" variant="filled" name="artist" onChange={this.handleChange} type="text"/><br/>
+            <label htmlFor=""> </label>
+            <TextField id="filled-basic" label="Title" variant="filled" name="title" onChange={this.handleChange} type="text"/><br/>
+            <label htmlFor=""> </label>
+            <TextField id="filled-basic" label="Album" variant="filled" name="album" onChange={this.handleChange} type="text"/><br/>
+            <label htmlFor=""> </label>
+            <TextField id="filled-basic" label="Genre" variant="filled" name="genre" onChange={this.handleChange} type="text"/><br/>
+            <label htmlFor=""> </label>
+            <TextField id="filled-basic" label="Release Date" variant="filled" name="release_date" onChange={this.handleChange} type="text"/><br/><br/>
+            <Button variant="contained" type="submit">Create Song</Button>
           </form>
-        </div>
+        </Grid>
       </CreatesongFormDiv>
 
     );
